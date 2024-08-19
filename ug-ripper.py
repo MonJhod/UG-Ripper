5294bc83ff6ad98d62bf5f4e5d81d9b521926df5
John Dominguez <john.dominguez101@gmail.com>
2024-08-19 13:48:45 -0400

    first commit

---
+++ ug-ripper.py
@@ -0,0 +1,153 @@
+import configparser
+import getpass
+import logging
+import os
+import pdfkit
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# Read configuration from file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Get download location from config
+download_location = config.get('Download', 'location', fallback='.')
+
+# Setup pdfkit config pointing to executable
+pdfkitConfig = pdfkit.configuration(wkhtmltopdf=config.get('PDFKit', 'executable_path'))
+
+# List to keep track of failed downloads
+failed_songs = []
+
+# Function to prompt for credentials
+def get_credentials():
+    username = input("Enter your username: ")
+    password = getpass.getpass("Enter your password: ")
+    return username, password
+
+# Function to set up WebDriver with download location
+def setup_webdriver(download_location):
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("browser.download.folderList", 2)
+    profile.set_preference("browser.download.manager.showWhenStarting", False)
+    profile.set_preference("browser.download.dir", os.path.abspath(download_location))
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/pdf")
+    
+    options = webdriver.FirefoxOptions()
+    options.profile = profile
+    options.add_argument("--headless")
+    options.enable_downloads = False
+    
+    driver = webdriver.Firefox(options=options)
+    return driver
+
+# Function to log in to Ultimate Guitar
+def login(username, password):
+    login_url = config.get('URLs', 'login_url')
+    driver = setup_webdriver(download_location)
+    driver.get(login_url)
+    
+    try:
+        login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[span[text()='Log in']]")))
+        login_button.click()
+        
+        username_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'username')))
+        password_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
+        
+        username_field.send_keys(username)
+        password_field.send_keys(password)
+        password_field.send_keys(Keys.RETURN)
+        
+        WebDriverWait(driver, 5).until(lambda d: d.get_cookie('bbusername') is not None)
+        
+        if driver.get_cookie('bbusername') is None:
+            logging.error("Login failed.")
+            driver.quit()
+            return None
+        else:
+            logging.info("Login successful.")
+            return driver
+    except TimeoutException:
+        logging.error("Timeout occurred while waiting for elements.")
+        driver.quit()
+        return None
+
+# Function to parse song links from the playlist page
+def parse_song_links(driver):
+    playlist_url = config.get('URLs', 'playlist_url')
+    driver.get(playlist_url)
+    
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        song_links = soup.find_all('a', href=True)
+        song_urls = [link['href'] for link in song_links if 'tabs.ultimate-guitar.com/tab' in link['href'] or 'https://tabs.ultimate-guitar.com/user/tab' in link['href']]
+        return song_urls
+    except TimeoutException:
+        logging.error("Timeout occurred while waiting for song links.")
+        return []
+
+def download_pdf(driver, song_url):
+    driver.get(song_url)
+    
+    try:
+        # Wait for the page to load
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[3]/main/div[2]/article[1]/section[2]/article/div")))
+        
+        # Extract song title
+        song_title_element = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/main/div[2]/article[1]/section[1]/header")
+        song_title = song_title_element.text.strip().replace('/', '_')    
+        # Download everything the content xpaths 
+        song_html = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/main/div[2]/article[1]/section[2]/article/div").get_attribute('outerHTML')
+    
+        # Concatenate song_title_element and song_html for the full download_html
+        download_html = song_title_element.get_attribute('outerHTML') + song_html        
+        # Convert HTML to PDF
+        pdfkit.from_string(download_html, f"{download_location}/{song_title}.pdf", configuration=pdfkitConfig)
+        
+        logging.info(f"HTML downloaded for song: {song_title}")
+    except (TimeoutException, NoSuchElementException) as e:
+        logging.error(f"Failed to download HTML for song: {driver.title.split(' | ')[0]}")
+        logging.error(str(e))
+        failed_songs.append(song_url)
+        
+# Function to fetch and save song data
+def fetch_songs(driver):
+    song_urls = parse_song_links(driver)
+    
+    total_songs = len(song_urls)
+    for index, song_url in enumerate(song_urls, start=1):
+        logging.info(f"Processing song {index}/{total_songs}")
+        download_pdf(driver, song_url)
+
+if __name__ == "__main__":
+    while True:
+        # Check if the config contains an Authentication section with Username and Password
+        if config.has_section('Authentication') and config.has_option('Authentication', 'Username') and config.has_option('Authentication', 'Password'):
+            username = config.get('Authentication', 'Username')
+            password = config.get('Authentication', 'Password')
+        else:
+            username, password = get_credentials()
+            
+        driver = login(username, password)
+        if driver:
+            fetch_songs(driver)
+            driver.quit()
+            break
+        else:
+            logging.error("Login failed. Please try again.")
+            
+    if failed_songs:
+        logging.info("Failed to download the following songs:")
+        for song in failed_songs:
+            logging.info(song)
+    
